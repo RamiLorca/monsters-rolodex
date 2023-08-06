8b9dc6523e86adc95a23bcf1e49cf3b1726b8bb5
Rami Lorca <ramilorca@Ramis-MBP.cable.rcn.com>
2023-08-05 23:36:07 -0400

    SearhcBox and CardList components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import './App.css';
 import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
 
@@ -42,13 +43,12 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input 
-        className='search-box'  
-        type='search' 
-        placeholder='search monsters' 
-        onChange={onSearchChange}
+        <SearchBox 
+          onChangeHandler={onSearchChange} 
+          placeholder='search monsters'
+          className='search-box' 
         />
-          <CardList monsters={filteredMonsters}/>
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
