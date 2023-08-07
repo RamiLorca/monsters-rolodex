e52ee8d58f057d1764e152e0edcfe993d039647b
Rami Lorca <ramilorca@Ramis-MBP.cable.rcn.com>
2023-08-07 01:19:35 -0400

    completed functional component conversion

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-// import { Component } from 'react';
 import './App.css';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
 
   const [searchField, setSearchField] = useState(''); //[value, setValue]
-  console.log(searchField);
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
 
   const onSearchChange = (event) => {
-    const searchFieldString = event.target.value.toLowerCase();
+    const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   }
 
@@ -24,7 +38,7 @@ const App = () => {
         className='monsters-search-box' 
       />
 
-      {/* <CardList monsters={filteredMonsters} />  */}
+      <CardList monsters={filteredMonsters} /> 
     </div>
   );
 };
